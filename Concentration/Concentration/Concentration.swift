595716666642a49687a746c7561395ef3baff80a
madan <madan@student.21-school.ru>
2020-03-14 21:54:57 +0300

    added func chooseCard in Concrntration

--- Concentration/Concentration/Concentration.swift
+++ Concentration/Concentration/Concentration.swift
@@ -21,6 +21,29 @@ class Concentration {
         }
     }
     
+    private var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int)
+    {
+        if cards[index].isFaceUp {
+            cards[index].isFaceUp = false
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                for flipdown in cards.indices {
+                    cards[flipdown].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
     init(numbersOfPairCards: Int) {
         if numbersOfPairCards > 0 {
             for _ in 1...numbersOfPairCards {
